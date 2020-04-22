da62e1b979701d68e38ea5a81a0ad1fd8f772f8c
Jonathan Lee <jsl372@cornell.edu>
2020-04-22 14:00:05 -0700

    changes to readingjson

--- readingjson.ml
+++ readingjson.ml
@@ -6,32 +6,39 @@ type message = {
   sent_by:string;
   text:string;
 }
-
-type t = {
+(* 
+type t_convo = {
   convo:message list
-}
+} *)
 
 type convo = message list
+type contacts = string list
 
-type newt = {
-  all_convos : convo list;
-  contacts : string list;
-}
+(* type newt = {
+   convo: message list;
+   contacts : string list;
+   } *)
 
-let make_message mess ={
-  sent_by = mess |> member "sent_by" |> to_string;
-  text = mess |> member "text" |> to_string;
+let message_from_convo_json j ={
+  sent_by = j |> member "sent_by" |> to_string;
+  text = j |> member "text" |> to_string;
 }
 
 let rec make_message_list mess_list json_list=
   match json_list with
   |[]-> mess_list
-  |h::t-> make_message_list ((make_message h)::mess_list) t
+  |h::t-> make_message_list ((message_from_convo_json h)::mess_list) t
 
 (* description: takes in json script and pipelines script to string attributes,
    requires: json is valid json script *)
-let from_json json (name1:string) (name2:string)={
-  convo=json |> member (Jmodule.id_creator name1 name2) |> to_list |> make_message_list [];}
+let from_json json (name1:string) (name2:string)=
+  json |> member (Jmodule.id_creator name1 name2) |> to_list |> make_message_list []
+
+let convo_from_json json = 
+  json |> member "text history" |> to_list |> List.map message_from_convo_json
+
+let contacts_from_json json = 
+  json |> member "contact list" |> to_list |> List.map to_string 
 
 (* helper function: takes sent_by and text and displays message *)
 let output_convo_line message=
